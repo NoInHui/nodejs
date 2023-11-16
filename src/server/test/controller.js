9f41eaf58910814efe209b802159ae1648f7c9ae
NoInHui <aincg1001@naver.com>
2023-11-16 17:01:46 +0900

    모던 자바스크립트 Deep Dive

--- src/server/test/controller.js
+++ src/server/test/controller.js
@@ -1,3 +1,5 @@
+const fs = require('fs');
+
 exports.fetchTest = async function(req, res) {
     return res.status(200).json({ id: '200', message: 'success', result: '1'});
     // return res.status(400).json({ id: '400', message: 'error'});
@@ -22,4 +24,72 @@ exports.requestTest1 = async function(req, res) {
     console.log('requestTest1');
     console.log(req);
     return res.status(200).json({ id: '200', message: 'success', result: 'requestTest1'});
+}
+
+exports.getTodos = async function(req, res) {
+    try {
+        let id = req.params.id;
+        let fileInfo = fs.readFileSync(`public/save/todo.json`, 'utf8');
+        fileInfo = JSON.parse(fileInfo);
+        if(id) fileInfo = fileInfo.todos.find(v => v.id == id);
+        return res.status(200).json(fileInfo.todos);
+    } catch(e) {
+        console.log(e);
+        return res.status(400).json({ id: '400', message: 'error'});
+    }
+}
+
+exports.postTodos = async function(req, res) {
+    try {
+        let body = req.body;
+        let fileInfo = fs.readFileSync(`public/save/todo.json`, 'utf8');
+        fileInfo = JSON.parse(fileInfo);
+        fileInfo.todos.push(body);
+        return res.status(200).json(fileInfo.todos);
+    } catch(e) {
+        console.log(e);
+        return res.status(400).json({ id: '400', message: 'error'});
+    }
+}
+
+exports.putTodos = async function(req, res) {
+    try {
+        let id = req.params.id;
+        let body = req.body;
+        let fileInfo = fs.readFileSync(`public/save/todo.json`, 'utf8');
+        fileInfo = JSON.parse(fileInfo);
+        return res.status(200).json([body]);
+    } catch(e) {
+        console.log(e);
+        return res.status(400).json({ id: '400', message: 'error'});
+    }
+}
+
+exports.patchTodos = async function(req, res) {
+    try {
+        let id = req.params.id;
+        let body = req.body;
+        let fileInfo = fs.readFileSync(`public/save/todo.json`, 'utf8');
+        fileInfo = JSON.parse(fileInfo);
+        let searchTodo = fileInfo.todos.find(v => v.id == id);
+        if(searchTodo) searchTodo.complete = body.complete;
+
+        return res.status(200).json(fileInfo.todos);
+    } catch(e) {
+        console.log(e);
+        return res.status(400).json({ id: '400', message: 'error'});
+    }
+}
+
+exports.deleteTodos = async function(req, res) {
+    try {
+        let id = req.params.id;
+        let fileInfo = fs.readFileSync(`public/save/todo.json`, 'utf8');
+        fileInfo = JSON.parse(fileInfo);
+        fileInfo.todos = fileInfo.todos.filter(v => v.id != id);
+        return res.status(200).json(fileInfo.todos);
+    } catch(e) {
+        console.log(e);
+        return res.status(400).json({ id: '400', message: 'error'});
+    }
 }
--- src/server/test/router.js
+++ src/server/test/router.js
@@ -10,4 +10,12 @@ router.get('/fetchTest4', controller.fetchTest4);
 router.get('/requestTest1', controller.requestTest1);
 router.post('/requestTest1', controller.requestTest1);
 
+router.get('/todos', controller.getTodos);
+router.get('/todos/:id', controller.getTodos);
+router.post('/todos', controller.postTodos);
+router.put('/todos/:id', controller.putTodos);
+router.patch('/todos/:id', controller.patchTodos);
+router.delete('/todos/:id', controller.deleteTodos);
+
+
 module.exports = router;
